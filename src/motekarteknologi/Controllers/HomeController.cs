f414f0c70c907547bac5e41a72059f6f325a5dc8
ismail hamzah <gohamzah@outlook.com>
2018-03-03 17:41:07 +0700

    adminlte static files integration by copy paste to wwwroot

--- src/motekarteknologi/Controllers/HomeController.cs
+++ src/motekarteknologi/Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace motekarteknologi.Controllers
             return View();
         }
 
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
