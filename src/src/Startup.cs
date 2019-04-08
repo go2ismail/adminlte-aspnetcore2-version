276b1ee62308d72278fda06490424b7841feae23
António Campos <jantoniofcampos@sapo.pt>
2019-04-08 21:33:46 +0100

    typo

--- src/src/Startup.cs
+++ src/src/Startup.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
 using src.Data;
 using src.Models;
 using src.Services;
